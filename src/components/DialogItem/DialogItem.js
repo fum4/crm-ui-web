cab0fc763b68318f53ac2a5bac8d6227eceaad64
Notorious <cristee95@gmail.com>
2021-02-07 09:57:20 +0200

    fixed all autocomplete warnings and bugs

--- src/components/DialogItem/DialogItem.js
+++ src/components/DialogItem/DialogItem.js
@@ -1,13 +1,11 @@
 import {useEffect, useState} from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
-import _ from 'lodash';
 
 export default function DialogItem({classes, field, onInputChange}) {
   const isDate = field.id === 'date';
   const today = new Date().toISOString().slice(0, -8);
   const type = isDate ? 'datetime-local' : '';
-  const [init, setInit] = useState(false);
   const [value, setValue] = useState();
   const [inputValue, setInputValue] = useState('');
   const [optionsIDs, setOptionsIDs] = useState();
@@ -21,51 +19,54 @@ export default function DialogItem({classes, field, onInputChange}) {
     setValue(valueInit);
 
     if (isDropdown) {
-      const inputValueInit = field.options?.find((option) => option._id === field.value)?.label;
+      const inputValueInit = field.options?.find((option) => option._id === field.value)?.label || '';
       setInputValue(inputValueInit);
     }
 
     isDate && onInputChange('date', valueInit);
-
-    !init && setInit(true);
   }, [field]);
-  return init ? (
-    isDropdown ? (
-      <Autocomplete
-        className={classes.input}
-        disabled={field.isDisabled}
-        getOptionLabel={(item) => field.options?.find((option) => option._id === item)?.label}
-        inputValue={inputValue}
-        key={field.id}
-        onChange={(ev, value) => {
-          setValue(value);
-          onInputChange(field.id, value);
-        }}
-        onInputChange={(_, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        options={optionsIDs}
-        renderInput={(params) => <TextField {...params} label={field.label} variant='filled' />}
-        required={field.isRequired}
-        value={value}
-      />
-    ) : (
-      <TextField
-        className={classes.input}
-        id={field.id}
-        key={field.id}
-        label={field.label}
-        onChange={(event) => {
-          setValue(event.target.value);
-          onInputChange(field.id, event.target.value);
-        }}
-        required={field.isRequired}
-        type={type}
-        value={value}
-        variant='filled'
-      />
-    )
+
+  return isDropdown ? (
+    <Autocomplete
+      className={classes.input}
+      disabled={field.isDisabled}
+      getOptionLabel={(item) => field.options?.find((option) => option._id === item)?.label || ''}
+      getOptionSelected={(option, value) => {
+        //nothing that is put in here will cause the warning to go away
+        if (value === '') {
+          return true;
+        } else if (value === option) {
+          return true;
+        }
+      }}
+      inputValue={inputValue}
+      key={field.id}
+      onChange={(ev, value) => {
+        setValue(value);
+        onInputChange(field.id, value);
+      }}
+      onInputChange={(_, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      options={optionsIDs}
+      renderInput={(params) => <TextField {...params} label={field.label} variant='filled' />}
+      required={field.isRequired}
+      value={value}
+    />
   ) : (
-    <div></div>
+    <TextField
+      className={classes.input}
+      id={field.id}
+      key={field.id}
+      label={field.label}
+      onChange={(event) => {
+        setValue(event.target.value);
+        onInputChange(field.id, event.target.value);
+      }}
+      required={field.isRequired}
+      type={type}
+      value={value}
+      variant='filled'
+    />
   );
 }
