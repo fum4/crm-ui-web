ef00aa1a916db05db95104f469d96ad887fd8fc1
Alexandru Andronache <alexandru.andronache@iquestgroup.com>
2021-04-02 19:53:31 +0300

    Add filtering on Today screen

--- src/screens/Today/Today.js
+++ src/screens/Today/Today.js
@@ -23,7 +23,38 @@ const Clients = () => {
     setAppointments(filteredAppointments.length ? filteredAppointments : allAppointments);
   }, [allAppointments, filteredAppointments]);
 
-  const handleSearch = () => {};
+  const handleSearch = (payload) => {
+    const keywords = payload.split(' ');
+
+    const filtered = allAppointments.filter((item) => {
+      let isMatch = false;
+
+      if (item) {
+        keywords.forEach((keyword) => {
+          const name = item.name?.toLowerCase();
+          const surname = item.surname?.toLowerCase();
+          const treatment = item.treatment?.toLowerCase();
+          const technician = item.technician?.toLowerCase();
+          const appointment = item.appointment?.toLowerCase();
+          const date = item.date?.toLowerCase();
+
+          const searchTerm = keyword?.toLowerCase();
+          const searchPool = [name, surname, treatment, technician, appointment, date];
+
+          searchPool.forEach((searchItem) => {
+            if (searchItem?.includes(searchTerm)) {
+              isMatch = true;
+            }
+          })
+
+        });
+      }
+
+      return isMatch;
+    });
+
+    setFilteredAppointments(filtered);
+  };
 
   return (
     <>
