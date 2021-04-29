f5597073bb7ee7a00ea644457dfad3477a13cfe7
fum4 <andronache.alex22@gmail.com>
2021-04-29 23:20:33 +0300

    - fix Header in place when scrolled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -44,24 +44,38 @@ const BootstrapInput = withStyles((theme) => ({
 const Header = ({ handleSearch, type }) => {
   const [showModal, setShowModal] = useState(false);
 
+  window.addEventListener('scroll', () => {
+    const component = document.getElementsByClassName('wrapper')[0];
+
+    if (window.pageYOffset > 175) {
+      component.classList.add('fixed');
+    }
+
+    if (window.pageYOffset < 150) {
+      component.classList.remove('fixed');
+    }
+  });
+
   return (
     <>
-      <div className='search-input-container'>
-        <BootstrapInput
-          className='search-input-container__main-input'
-          onChange={(ev) => handleSearch(ev.target.value)}
-        />
-        <Button
-          className='add-new-btn'
-          color='primary'
-          onClick={() => setShowModal(true)}
-          size='large'
-          startIcon={type === 'client' ? <PersonAdd /> : <AddAlarm />}
-          variant='contained'>
-          {
-            !isMobile() && <p>{type === 'client' ? labels.ADD_CLIENT : labels.ADD_APPOINTMENT}</p>
-          }
-        </Button>
+      <div className='wrapper'>
+        <div className='search-input-container'>
+          <BootstrapInput
+            className='search-input-container__main-input'
+            onChange={(ev) => handleSearch(ev.target.value)}
+          />
+          <Button
+            className='add-new-btn'
+            color='primary'
+            onClick={() => setShowModal(true)}
+            size='large'
+            startIcon={type === 'client' ? <PersonAdd /> : <AddAlarm />}
+            variant='contained'>
+            {
+              !isMobile() && <p>{type === 'client' ? labels.ADD_CLIENT : labels.ADD_APPOINTMENT}</p>
+            }
+          </Button>
+        </div>
       </div>
       {showModal && (
         <Dialog action='add' setShowModal={setShowModal} type={type} />
