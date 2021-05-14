4bc6a89936636f56ca3c360ac07680194ad1db4e
fum4 <andronache.alex22@gmail.com>
2021-05-14 23:21:17 +0300

    - remove data.length dependency from fetch hooks

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchClients, fetchAppointments } from './';
 
-export const useAllClients = (isAuthenticated) => {
+export const useClients = (isAuthenticated) => {
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.clients);
 
@@ -15,7 +15,7 @@ export const useAllClients = (isAuthenticated) => {
   return data;
 }
 
-export const useAllAppointments = (isAuthenticated) => {
+export const useAppointments = (isAuthenticated) => {
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.appointments);
 
