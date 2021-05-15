c99ca54915b97ab5fcd52ecc750b304a1ee5320d
fum4 <andronache.alex22@gmail.com>
2021-05-16 00:10:02 +0300

    - do not display entries if search terms don't match any

--- src/screens/Today/Today.js
+++ src/screens/Today/Today.js
@@ -8,6 +8,7 @@ import {useSelector} from "react-redux";
 
 const Today = () => {
   const isAuthenticated = useSelector((state) => state.general.isAuthenticated);
+  const [isFiltering, setIsFiltering] = useState(false);
   const [filteredAppointments, setFilteredAppointments] = useState([]);
   const [appointments, setAppointments] = useState([]);
   const allAppointments = useAppointments(isAuthenticated);
@@ -20,59 +21,64 @@ const Today = () => {
   }, [history, isAuthenticated]);
 
   useEffect(() => {
-    setAppointments(filteredAppointments.length ? filteredAppointments : allAppointments);
-  }, [allAppointments, filteredAppointments]);
+    setAppointments(isFiltering ? filteredAppointments : allAppointments);
+  }, [allAppointments, isFiltering, filteredAppointments]);
 
   const handleSearch = (payload) => {
-    const keywords = payload.split(' ');
+    if (payload) {
+      const keywords = payload.split(' ');
 
-    const filtered = allAppointments.filter((item) => {
-      let isMatch = true;
+      const filtered = allAppointments.filter((item) => {
+        let isMatch = true;
 
-      if (item) {
-        const name = item.name?.toLowerCase();
-        const surname = item.surname?.toLowerCase();
-        const treatment = item.treatment?.toLowerCase();
-        const technician = item.technician?.toLowerCase();
-        const price = item.price?.toLowerCase();
-        const date = item.type === 'appointment' && item.appointment?.toLowerCase() || item.date?.toLowerCase();
-        const time = date.slice(date.indexOf('t') + 1);
-        const day = (+date.slice(8, 10)).toString();
-        const dayNameEn = moment(date).format('dddd').toUpperCase();
-        const dayNameRo = labels.DAYS[`${dayNameEn}_NO_SPECIAL_CHARS`];
-        const monthNumber = date.slice(5, 7);
-        const monthNameEn = moment(monthNumber, 'MM').format('MMMM').toUpperCase();
-        const monthNameRo = labels.MONTHS[monthNameEn];
-        let searchByDate = false
+        if (item) {
+          const name = item.name?.toLowerCase();
+          const surname = item.surname?.toLowerCase();
+          const treatment = item.treatment?.toLowerCase();
+          const technician = item.technician?.toLowerCase();
+          const price = item.price?.toLowerCase();
+          const date = item.type === 'appointment' && item.appointment?.toLowerCase() || item.date?.toLowerCase();
+          const time = date.slice(date.indexOf('t') + 1);
+          const day = (+date.slice(8, 10)).toString();
+          const dayNameEn = moment(date).format('dddd').toUpperCase();
+          const dayNameRo = labels.DAYS[`${dayNameEn}_NO_SPECIAL_CHARS`];
+          const monthNumber = date.slice(5, 7);
+          const monthNameEn = moment(monthNumber, 'MM').format('MMMM').toUpperCase();
+          const monthNameRo = labels.MONTHS[monthNameEn];
+          let searchByDate = false
 
-        keywords.forEach((keyword) => {
-          const searchTerm = keyword?.toLowerCase();
-          const searchPool = searchByDate
-            ? [monthNameRo, day, dayNameRo]
-            : [monthNameRo, name, surname, treatment, technician, price, time, day, dayNameRo];
+          keywords.forEach((keyword) => {
+            const searchTerm = keyword?.toLowerCase();
+            const searchPool = searchByDate
+                ? [monthNameRo, day, dayNameRo]
+                : [monthNameRo, name, surname, treatment, technician, price, time, day, dayNameRo];
 
-          let currentItemMatched = false;
+            let currentItemMatched = false;
 
-          searchPool.forEach((searchItem) => {
-            if (searchItem?.includes(searchTerm)) {
-              if (searchItem === monthNameRo) {
-                searchByDate = true;
+            searchPool.forEach((searchItem) => {
+              if (searchItem?.includes(searchTerm)) {
+                if (searchItem === monthNameRo) {
+                  searchByDate = true;
+                }
+
+                currentItemMatched = true;
               }
+            })
 
-              currentItemMatched = true;
+            if (!currentItemMatched) {
+              isMatch = false;
             }
-          })
+          });
+        }
 
-          if (!currentItemMatched) {
-            isMatch = false;
-          }
-        });
-      }
+        return isMatch;
+      });
 
-      return isMatch;
-    });
-
-    setFilteredAppointments(filtered);
+      setIsFiltering(true);
+      setFilteredAppointments(filtered);
+    } else {
+      setIsFiltering(false);
+    }
   };
 
   return isAuthenticated && (
