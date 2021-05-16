7865a6bdc2a5dd55f7d883a5f3c64510701de097
fum4 <andronache.alex22@gmail.com>
2021-05-17 01:04:02 +0300

    clean up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './store';
 import { Provider } from 'react-redux';
-import './index.css';
+import { store } from 'store';
+import reportWebVitals from 'reportWebVitals';
+import App from 'App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
